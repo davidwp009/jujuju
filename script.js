fb96e6062fb488084777344469f7964d1ab496ee
davidwp009 <142822982+davidwp009@users.noreply.github.com>
2025-02-24 18:32:31 +0200

    cdsa

--- script.js
+++ script.js
@@ -1,15 +1,74 @@
-(() => { 
-  const refs = { 
-    openModalBtn: document.querySelector("[data-modal-open]"), 
-    closeModalBtn: document.querySelector("[data-modal-close]"), 
-    modal: document.querySelector("[data-modal]"), 
-  }; 
- 
-  refs.openModalBtn.addEventListener("click", toggleModal); 
-  refs.closeModalBtn.addEventListener("click", toggleModal); 
- 
-  function toggleModal() { 
-    refs.modal.classList.toggle("is-hidden"); 
-    document.body.classList.toggle("no-scroll"); 
-  } 
-})();
+const a = 20;
+const b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+
+// оператори порівняння
+
+console.log(a > b);
+console.log(a < b);
+a >= b;
+a <= b;
+a == b;
+a != b;
+console.log(a === b);
+console.log(a !== b);
+
+
+5 == '5' //true
+5 === '5' //false
+
+0 == false
+0 === false
+
+//const c = '20';
+//console.log(Number(c))
+//console.log(typeof Number(d))
+
+
+console.log(Number.parseInt('10px'));
+console.log(Number.parseInt('15qwe24'));
+console.log(Number.parseInt('14.45qwe'));
+console.log(Number.parseInt('qwe'));
+
+
+console.log(Number.parseFloat('10px'));
+console.log(Number.parseFloat('12qwe74'));
+console.log(Number.parseFloat('12.24qwe78'));
+
+
+const x = Number( '25' );
+console.log(Number.isNaN(x));
+
+const y = Number( 'qweqwe' );
+console.log(Number.isNaN(y));
+
+0.3 + 0.05
+0.3//0.
+
+
+//console.log(0. + 0.3 === 0.4);
+console.log(0.1 + 0.3);
+
+//console(Math.floor(20.8));
+//console(Math.ceil(3.4));
+//console(Math.round(1.2));
+//console(Math.round(1.5));
+
+
+//console(Math.max(20, 100, 47, 124, 99));
+//console(Math.min(85, 24, 78, 12));
+
+//console(Math.pow(2, 3));
+
+//console(Math.random() * (10 - 1) + 1);
+
+const k1 = Number.parseFloat('5px');
+const k2 = Number.parseFloat('12djd334');
+const k3 = Number.parseFloat('12.45asdwe12');
+const k4 = Number.parseFloat('qwqweeewq');
+console.log(k1, k2, k3, k4)
